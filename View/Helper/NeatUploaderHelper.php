826d2d145c7dd283ee4fa9689c8858035a443ef5
motses <SnapEvent@MacBook-Pro-de-Olivier-2.local>
2014-10-22 20:15:22 +0200

    bug

--- View/Helper/NeatUploaderHelper.php
+++ View/Helper/NeatUploaderHelper.php
@@ -3,8 +3,8 @@ class NeatUploaderHelper extends AppHelper{
 	public $helpers = array("Html");
 
 	public function init(){
-		$toreturn = $this->Html->css("/neat_uploader/css/uploader/neat-little-uploader.css",array("plugin" => true));
-		$toreturn.= $this->Html->script("/neat_uploader/js/uploader/neat-little-uploader.js",array("plugin" => true));
+		$toreturn = $this->Html->css("/neat_uploader/css/neat-little-uploader.css",array("plugin" => true));
+		$toreturn.= $this->Html->script("/neat_uploader/js/neat-little-uploader.js",array("plugin" => true));
 		return $toreturn;
 	}
 
